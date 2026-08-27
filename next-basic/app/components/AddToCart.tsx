66cca65d035a6d5df7393196c01e7cf59dbc181a
Vivek <krishnacreations745@gmail.com>
2026-08-27 19:47:16 +0530

    intro to next

---
+++ next-basic/app/components/AddToCart.tsx
@@ -0,0 +1,16 @@
+"use client";
+import React from "react";
+
+const AddToCart = () => {
+  return (
+    <button
+      onClick={() => {
+        console.log("hello");
+      }}
+    >
+      Add TO cart
+    </button>
+  );
+};
+
+export default AddToCart;
--- next-basic/app/page.tsx
+++ next-basic/app/page.tsx
@@ -1,7 +1,12 @@
+import Link from "next/link";
+import ProductCard from "./components/ProductCard";
+
 const Home = () => {
   return (
     <main>
       <h1>Hello World</h1>
+      <Link href="/users">Users</Link>
+      <ProductCard />
     </main>
   );
 };
